140074eada695df86f1f723a739d0f3dbf8eb33d
espura0120-maker <espura0120@gmail.com>
2026-04-09 14:16:49 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,12 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl  = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey  = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false
+export const supabase = createClient(
+  'https://pzezpdlnpyozomqgtltt.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6ZXpwZGxucHlvem9tcWd0bHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NDUwNjcsImV4cCI6MjA5MTIyMTA2N30.zt-AOMqpuz0cuZrWxdXhC6KqKwmhBcHUPz2zH9pcCGE',
+  {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false
+    }
   }
-})
+)
